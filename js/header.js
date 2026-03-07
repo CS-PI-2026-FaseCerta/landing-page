59637522d00c72c7c2d7cbd4de5972fb20195e06
Francisco <simoesdeoliveirafrancisco@gmail.com>
2026-03-06 22:38:35 -0300

    feat-alteracao-fontSize

--- js/header.js
+++ js/header.js
@@ -25,3 +25,28 @@ function imagemAnterior() {
   imgAtual = Math.max(0, imgAtual - 1);
   viewport.scrollTo({ left: imgAtual * w, behavior: "smooth" });
 }
+
+let tamanho = 0;
+
+function alterarFontSize() {
+  tamanho++;
+  if (tamanho > 2) {
+    tamanho = 0;
+  }
+  switch (tamanho) {
+    case 0:
+      document.documentElement.style.setProperty("--fs-p", "1.25rem");
+      document.documentElement.style.setProperty("--fs-a", "1.25rem");
+      break;
+    case 1:
+      document.documentElement.style.setProperty("--fs-p", "1.50rem");
+      document.documentElement.style.setProperty("--fs-a", "1.50rem");
+      break;
+    case 2:
+      document.documentElement.style.setProperty("--fs-p", "1.75rem");
+      document.documentElement.style.setProperty("--fs-a", "1.75rem");
+      break;
+  }
+
+  console.log(document.documentElement.style.getPropertyValue("--fs-p"));
+}
